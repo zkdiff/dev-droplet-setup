3df26be67c5505989f7efdc3d6cf552cdf9f2f8e
zkdiff <37495954+zkdiff@users.noreply.github.com>
2026-02-28 00:22:14 -0800

    feat: add GitHub CLI installation to the bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -90,6 +90,15 @@ log "Installing zsh..."
 wait_for_apt
 DEBIAN_FRONTEND=noninteractive apt-get install -y -qq zsh
 
+(type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+	&& cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& sudo mkdir -p -m 755 /etc/apt/sources.list.d \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
 
 # Change default shell to zsh
 chsh -s "$(which zsh)" root
